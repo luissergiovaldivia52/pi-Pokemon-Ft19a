ed69b4e45504c924af2007ab6c09380cbff7b180
luissergiovaldivia <58519370+luissergiovaldivia52@users.noreply.github.com>
2021-11-25 19:23:33 -0300

    backend get detalle with DB

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -15,12 +15,12 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
-    id:{
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV1,
-      allowNull:false,
-      primaryKey:true
-    },
+    //id:{
+    //  type: DataTypes.UUID,
+    //  defaultValue: DataTypes.UUIDV1,
+    //  allowNull:false,
+    //  primaryKey:true
+    //},
     name: {
       type: DataTypes.STRING,
       allowNull: false,
