e87d427d1674783d0f49b4e04e373c6296744789
luissergiovaldivia <58519370+luissergiovaldivia52@users.noreply.github.com>
2021-12-09 17:31:52 -0300

    correcion errores 3

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 
 
